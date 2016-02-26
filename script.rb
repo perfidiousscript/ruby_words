4c82ce4bd71027ff9bc947d2b37868f87bdef83f
Sam Moss <sam.moss321@gmail.com>
2016-02-26 14:48:50 -0600

    creates a hash of sequences with values of sequence, source word and is_duplicate

--- script.rb
+++ script.rb
@@ -6,22 +6,45 @@ word = file.read
 
 $wordObject = {}
 $wordArray = []
+$seqList = {}
 
 def arrayConvert(wordListString)
   $wordArray = wordListString.split("\n")
-  print "word array: ", $wordArray
+  #print "word array: ", $wordArray
   return $wordArray
 end
 
 def objectStuff(wordArray)
-  $wordArray.each do |word|
-    $wordObject[word.to_sym] = {
-      :word => word,
-      :seq => []
+  wordArray.each do |word|
+    $wordObject[word] = {
+      word: word,
+      "seq" => word.split("")
       }
   end
-  puts "\n word Object: ", $wordObject.inspect
+  #puts "\n word Object: ", $wordObject.inspect
+end
+
+def perm(objectList)
+  objectList.each do |word, value|
+    #puts "word is: ", value["seq"]
+    if value["seq"].length > 3
+      for i in 0...(value["seq"].length - 3)
+        newSeq = ""
+        newSeq = value["seq"][i] + value["seq"][i+1] + value["seq"][i+2] + value["seq"][i+3]
+        if $seqList[newSeq.to_sym]
+          $seqList[newSeq.to_sym]["is_duplicate"] = true
+        else
+          $seqList[newSeq.to_sym] = {
+            "seq" => newSeq,
+            "word" => word
+          }
+        end
+      end
+    end
+  end
 end
 
 arrayConvert(word)
 objectStuff($wordArray)
+perm($wordObject)
+puts "here is seqList: ", $seqList
